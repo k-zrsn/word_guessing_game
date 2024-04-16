05d1c0749b0a088f62f72b9cadea15f59b7fe761
vpadua <vgcpad@gmail.com>
2024-04-16 18:02:40 -0400

    created word bank, choose random word, and player count input

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,3 +1,23 @@
 ### word guessing game 
 
 import random
+
+print("Let's play a game of word guessing!")
+
+
+### a word bank containing 10 different words
+word_bank = ["great", "trace", "watch", "train", "phone", "blank", "short", "stamp", "magic", "smart"]
+
+chosen_word = random.choice(word_bank)
+
+print("Hint: all words are 5 letters long!")
+
+
+### how many players are playing?
+player_count = int(input("How many players are playing? "))
+
+
+
+print("The program has chosen its word!")
+
+#daniel lane
