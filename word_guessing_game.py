e1d2a824660a0971488e98b4a09253f11f7d48c0
vpadua <vgcpad@gmail.com>
2024-04-23 15:11:34 -0400

    i think im done?

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,23 +1,14 @@
-#daniel lane
 ### word guessing game 
 
 import random
 import time
 
-######
-
-
-############ MAKE LETTER REPEAT CHECK AND WORK ON MISTAKE COUNTER ###########
-
-
-####
-
-players = []
+player_list = []
 correct_letters = []
 incorrect_letters = []
 
 def choose_word():
-    word_bank = ["great", "trace", "watch", "train", "phone", "blank", "short", "stamp", "magic", "smart", "floccinaucinihilipilification"]
+    word_bank = ["great", "trace", "watch", "train", "phone", "blank", "short", "stamp", "magic", "smart"]
     return random.choice(word_bank)
 
 
@@ -26,30 +17,29 @@ def create_scoreboard(player_count):
     for i in range(1, player_count + 1):
         player_name = input(f"\nEnter name for Player {i}: ").upper()
         scoreboard[player_name] = 0
-        players.append(player_name)
+        player_list.append(player_name)
     return scoreboard
 
 
 def next_turn(player_name):
-    attempts = 0
+    letter_guesses = 0
     print(f"\nIt's {player_name}'s turn.")
-    return attempts + 1
+    return letter_guesses + 1
 
 
 def play():
     play_loop = True
     while play_loop:
-        mistakes = {player : 3 for player in players}
-        print (mistakes)
-        while mistakes != -1:
-            for player in players: 
-                if mistakes[player] == 0: 
+        incorrect_guess = {player : 3 for player in player_list}
+        while incorrect_guess != -1:
+            for player in player_list: 
+                if incorrect_guess[player] == 0: 
                     print(f"\n\nThat was your last guess. You lost!")
                     print(f"\nThe word was: {word}")
-                
-                    print("\nAttempts:")
-                    for player, attempts in scoreboard.items():
-                        print(f"{player}: {attempts}")
+            
+                    print("\nNumber of guesses:")
+                    for player, letter_guesses in scoreboard.items():
+                        print(f"{player}: {letter_guesses}")
                     play_loop = False
                     quit()
 
@@ -59,21 +49,8 @@ def play():
                 print(f"\nCorrect letters guessed so far: {correct_letters}")
                 print(f"\nIncorrect letters guessed so far: {incorrect_letters}")
                 
-                guess = input("\n\nGuess a letter: ").upper()
-
-                if guess in correct_letters or guess in incorrect_letters:
-                    print("\n\nYou already guessed that letter!")
-                    time.sleep(2)
-                    continue
-
-                if guess in word:
-                    print("\n\nYou guessed a letter!")
-                    #for guess in word:
-                    letter_count = word.count(guess)
-                    print(f"\nThe letter: {guess} appears in the word {letter_count} time(s).")
-                    correct_letters.append(guess)
-                    correct_letters.sort()
-
+                if correct_letters != []:
+                    time.sleep(1)
                     guess_word = input("\n\nWould you like to guess the word? (y/n): ").lower()
 
                     if guess_word == "y":
@@ -82,32 +59,53 @@ def play():
                             time.sleep(2)
                             print(f"\n\n\nCongratulations! {player} won!")
                             print(f"\nThe word was: {word}")
-                            print("\nAttempts:")
-                            for player, attempts in scoreboard.items():
-                                print(f"{player}: {attempts}")
+                            print("\nNumber of guesses:")
+                            for player, letter_guesses in scoreboard.items():
+                                print(f"{player}: {letter_guesses}")
                             quit()
-                            
                         else:
                             time.sleep(2)
                             print("\n\nSorry, that is not the word.")
-                            mistakes[player] -= 1
-                            print(f"\n{player} has {mistakes[player]} mistake(s) left\n\n")
+                            incorrect_guess[player] -= 1
+                            print(f"\n{player} has {incorrect_guess[player]} mistake(s) left\n\n")
 
+                    time.sleep(1)
+                
+                guess = input("\n\nGuess a letter: ").upper()
+
+                if guess in correct_letters or guess in incorrect_letters:
+                    print("\n\nThat letter has already been guessed!\n\n")
+                    time.sleep(2)
+                    continue
+
+                if guess in word:
+                    print("\n\nYou guessed a letter!")
+                    letter_count = word.count(guess)
+                    time.sleep(1)
+                    print(f"\nThe letter: {guess} appears in the word {letter_count} time(s).\n")
+                    correct_letters.append(guess)
+                    correct_letters.sort()
                     time.sleep(2)
                 else:
                     print("\nSorry, that letter is not in the word.\n\n")
                     incorrect_letters.append(guess)
                     incorrect_letters.sort()
-
-
                     time.sleep(2)
                 
-
-
                 ### prints scoreboard
-                print("\nAttempts:")
-                for player, attempts in scoreboard.items():
-                    print(f"{player}: {attempts}")
+                print("\nNumber of guesses:")
+                for player, letter_guesses in scoreboard.items():
+                    print(f"{player}: {letter_guesses}")
+
+                if letter_guesses == 2:
+                    give_up = input("\n\nWould you like to give up? (y/n): ").lower()
+                    if give_up == "y":
+                        time.sleep(1)
+                        print(f"\n\nThe word was: {word}")
+                        print("\nNumber of guesses:")
+                        for player, letter_guesses in scoreboard.items():
+                            print(f"{player}: {letter_guesses}\n")
+                        quit()
 
 
 word = choose_word().upper()
@@ -119,12 +117,12 @@ print("\n\nThe scoreboard is set!")
 
 time.sleep(0.5)
 ### prints scoreboard
-print("\nAttempts:")
-for player, attempts in scoreboard.items():
-    print(f"{player}: {attempts}")
+print("\nNumber of guesses:")
+for player, letter_guesses in scoreboard.items():
+    print(f"{player}: {letter_guesses}")
 
 time.sleep(0.5)
-print("\nWhoever has the least attempts wins...")
+print("\nWhoever has the least number of guesses wins...")
 
 time.sleep(1)
 print("\nBut whoever guesses the word first wins!\n")
