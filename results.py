2a729befec5ab54fbb08ffe3e911ae3534a7e3af
vpadua <vgcpad@gmail.com>
2024-05-12 17:24:42 -0400

    made small time changes in results.py

--- results.py
+++ results.py
@@ -9,10 +9,11 @@ def game_results(word, scoreboard):
     A function that prints the results of the game
     """
     print(f"\nThe word was: {word}")
-    time.sleep(1)
+    time.sleep(2)
     ### print scoreboard
     print("\nNumber of guesses:")
     for player, letter_guesses in scoreboard.items():
         print(f"{player}: {letter_guesses}")
+    time.sleep(2)
     print("\n\nThanks for playing!\n\n\n")
     quit()
